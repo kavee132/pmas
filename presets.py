cd31ece0bb8a33325e42654f2526311d211fa147
kavee132 <79425575+kavee132@users.noreply.github.com>
2021-09-08 17:39:14 +0530

    Add files via upload

---
+++ presets.py
@@ -0,0 +1,15 @@
+class Presets(object):
+    WELCOME_TEXT = "Heya, <b>{}</b>\n<code>I am a PM Assistant Bot created by TeLe TiPs\nSend your message to my master👇🏻</code> "
+    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+
+
+
+
+
+
+
+
+
+
